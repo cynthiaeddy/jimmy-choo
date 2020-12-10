231c6d201213a34b1c2e209f463386e37f312d5a
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-12-09 19:59:17 -0500

    added social to jim page

--- src/components/aboutJim/AboutJim.js
+++ src/components/aboutJim/AboutJim.js
@@ -52,6 +52,23 @@ const AboutJim = () => {
               <a href="#"><img src={jim }></img></a>
             </div>
           </div>
+
+          <div className='social'>
+            <div className='email'>
+            <a href='mailto:james@jamesoseland.com'><i class="fas fa-envelope fa-2x"></i></a>
+
+
+             </div>
+            <div className='instagram'>
+            <a href='https://www.instagram.com/jamesoseland'><i class="fab fa-instagram-square fa-2x"></i></a>
+
+            </div>
+            <div className='facebook'>
+            <a href=' https://www.facebook.com/james.oseland'><i class="fab fa-facebook fa-2x"></i></a>
+
+            </div>
+          </div>
+
         </div>
         </div>
       </div>
