5c30f2c771f0993f25626faf691ef62ea7bd2d7b
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-12-24 17:37:25 -0500

    home mobile nav readded

--- src/components/home/HomeMobile.js
+++ src/components/home/HomeMobile.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Carousel } from 'react-responsive-carousel';
 import NavbarHome from '../navbarHome/NavbarHome'
+import NavbarHomeMobile from '../navbarHome/NavbarHomeMobile'
+
 
 
 import mexicoCover from '../../assets/images/home/mexicoCover.jpg'
@@ -29,67 +31,92 @@ import orange2 from '../../assets/images/home/orange2.jpg'
 import '../../stylesheets/HomeMobile.css'
 
 
-const HomeMobile = () => (
-  <div className='mobile-container'>
-    <NavbarHome/>
+class HomeMobile extends React.Component {
+  state = {
+    width: window.innerWidth
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+  render() {
+
+    let isMobile;
+    let width = this.state.width;
+    width <= 539 ? (isMobile = true) : (isMobile = false);
+    return (
+    <div className='mobile-container'>
+        {isMobile ? <NavbarHomeMobile /> : <NavbarHome />
+        }
 
 
-    < div className='mobile'>
-      <div className='ipadhead'>
-        <h3>James Oselands's</h3>
-        <h1>World Food</h1>
-        <h5>Introducing World Food, an exceptional new series in the tradition of classic cookbooks that take you on a journey to the world's greatest cuisines and the remarkable cultures they come from.</h5>
-      </div>
-<section className='main-mobile'>
-        <div className='ipadtl'>
-          <img src={gal} className='gal' alt=''></img>
+      < div className='mobile'>
+        <div className='ipadhead'>
+          <h3>James Oselands's</h3>
+          <h1>World Food</h1>
+          <h5>Introducing World Food, an exceptional new series in the tradition of classic cookbooks that take you on a journey to the world's greatest cuisines and the remarkable cultures they come from.</h5>
         </div>
-        <div className='ipadml'>
-          <img src={corn} className='flan' alt=''></img>
-        </div>
-        <div className='ipadbl'>
-          <img src={man} className='corn' alt=''></img>
-        </div>
+        <section className='main-mobile'>
+          <div className='ipadtl'>
+            <img src={gal} className='gal' alt=''></img>
+          </div>
+          <div className='ipadml'>
+            <img src={corn} className='flan' alt=''></img>
+          </div>
+          <div className='ipadbl'>
+            <img src={man} className='corn' alt=''></img>
+          </div>
 
-      <div className='carousel-mobile'>
+          <div className='carousel-mobile'>
 
-        <Carousel
-          showThumbs={false}
-          infiniteLoop
-          autoPlay
-          interval={4000}
-          transitionTime={1000}
-          showIndicators={false}
-          showStatus={false}
-          showArrows={false}
-          >
+            <Carousel
+              showThumbs={false}
+              infiniteLoop
+              autoPlay
+              interval={4000}
+              transitionTime={1000}
+              showIndicators={false}
+              showStatus={false}
+              showArrows={false}
+            >
 
-            <div>
-              <img src={mexicoCover} className='mexico' alt=''></img></div>
-            <div>
-              <img src={parisCover} className='paris' alt=''></img>
-            </div>
-          </Carousel>
-        </div>
+              <div>
+                <img src={mexicoCover} className='mexico' alt=''></img></div>
+              <div>
+                <img src={parisCover} className='paris' alt=''></img>
+              </div>
+            </Carousel>
+          </div>
 
 
-        <div className='ipadtr'>
-          <img src={waiter} className='gal' alt=''></img>
-        </div>
-        <div className='ipadmr'>
-          <img src={orange2} className='gal' alt=''></img>
-        </div>
-        <div className='ipadbr'>
-          <img src={galred} className='gal' alt=''></img>
-        </div>
+          <div className='ipadtr'>
+            <img src={waiter} className='gal' alt=''></img>
+          </div>
+          <div className='ipadmr'>
+            <img src={orange2} className='gal' alt=''></img>
+          </div>
+          <div className='ipadbr'>
+            <img src={galred} className='gal' alt=''></img>
+          </div>
         </section>
 
-      <div className='mobile-footer'>
-        <h2 ><a href="https://jamesroper.photoshelter.com/index">Photography by James Roper</a></h2>
-        <button className='signUpBtn mobile'><h7>SIGN UP NOW!</h7></button>
-      </div>
+        <div className='mobile-footer'>
+          <h2 ><a href="https://jamesroper.photoshelter.com/index">Photography by James Roper</a></h2>
+          <button className='signUpBtn mobile'><h7>SIGN UP NOW!</h7></button>
+        </div>
 
+      </div>
     </div>
-  </div>
-  );
+  )
+  }
+}
   export default HomeMobile
