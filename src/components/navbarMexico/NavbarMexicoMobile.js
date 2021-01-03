17504560feacbfe49f7f5848b2e64ae852816938
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-01-03 15:07:38 -0500

    styled

--- src/components/navbarMexico/NavbarMexicoMobile.js
+++ src/components/navbarMexico/NavbarMexicoMobile.js
@@ -11,17 +11,15 @@ const NavbarMexicoMobile = () => {
 
     <nav id="navbarMexicoMobile" >
 
-{/* <div className='topnav'> */}
       <Link to="/navbar" id='menu'><h6>Menu</h6></Link>
-{/* <div className='secondary-nav'> */}
+      <div className='mexico-right'>
         <h6>James Oseland’s <br/><span class='nav-world'>WORLD FOOD</span></h6>
-        {/* </div> */}
 
       <div className='buyNav mexico'>
         <a href="https://www.amazon.com/World-Food-Heritage-Recipes-Classic/dp/0399579850">Purchase the Book</a>
-        </div>
+      </div>
+      </div>
 
-{/* </div> */}
 
 		</nav>
   );
