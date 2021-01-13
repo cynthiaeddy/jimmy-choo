2cd901da7c6aa8fc9d158a894e3a75665914aa17
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-01-12 21:38:45 -0500

    added igmur books urls

--- src/components/aboutJim/stash/AboutJimMobileFotos.js
+++ src/components/aboutJim/stash/AboutJimMobileFotos.js
@@ -3,6 +3,13 @@ import NavbarSocial from '../navbars/navbarSocial/NavbarSocial'
 import NavbarJimMobile from '../navbars/navbarJim/NavbarJimMobile'
 
 
+import mexico from '../../assets/images/books/mexico.png'
+import paris from '../../assets/images/books/paris.png'
+import fork from '../../assets/images/books/fork.png'
+import jim from '../../assets/images/books/jim.png'
+import cradle from '../../assets/images/books/cradle.png'
+import comfort from '../../assets/images/books/comfort.png'
+
 import '../../stylesheets/AboutJim.css'
 
 
@@ -26,41 +33,41 @@ class AboutJimMobile extends React.Component {
             </div>
             <div className='mobile-jim-right'>
               <div className='book blt'>
-                <a href="https://www.amazon.com/World-Food-Heritage-Recipes-Classic/dp/0399579850/ref=sr_1_1?crid=3PCZYLGIN4PLK&dchild=1&keywords=world+food+mexico+city&qid=1607723856&sprefix=world+food+me%2Caps%2C252&sr=8-1"><img src="https://imgur.com/XqQMbir.png" alt='mexico city book cover'></img>
+                <a href="https://www.amazon.com/World-Food-Heritage-Recipes-Classic/dp/0399579850/ref=sr_1_1?crid=3PCZYLGIN4PLK&dchild=1&keywords=world+food+mexico+city&qid=1607723856&sprefix=world+food+me%2Caps%2C252&sr=8-1"><img src={mexico} alt='mexico city book cover'></img>
                   <div class="middle ">
                     <div class="text">buy</div>
                   </div></a>
               </div>
 
               <div className='book bmt'>
-                <a href="https://www.amazon.com/World-Food-Heritage-Recipes-Classic/dp/0399579834/ref=sr_1_2?dchild=1&keywords=world+food+paris&qid=1607724226&s=books&sr=1-2"><img src="https://imgur.com/FmvS9sX.png" alt='paris book cover'></img>
+                <a href="https://www.amazon.com/World-Food-Heritage-Recipes-Classic/dp/0399579834/ref=sr_1_2?dchild=1&keywords=world+food+paris&qid=1607724226&s=books&sr=1-2"><img src={paris}></img>
                   <div class="middle">
                     <div class="text">buy</div>
                   </div></a>
               </div>
 
               <div className='book brt'>
-                <a href="https://www.amazon.com/Saveur-Comfort-Cooking-Oseland-2011-04-20/dp/B011KD0JUG/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=&sr="><img src="https://imgur.com/aer74Rk.png" lt='saveur-comfort book cover'></img>
+                <a href="https://www.amazon.com/Saveur-Comfort-Cooking-Oseland-2011-04-20/dp/B011KD0JUG/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=&sr="><img src={comfort}></img>
                   <div class="middle">
                     <div class="text">buy</div>
                   </div></a>
               </div>
 
               <div className='book blb'>
-                <a href="https://www.amazon.com/gp/product/0393054772/ref=dbs_a_def_rwt_bibl_vppi_i4"><img src="https://imgur.com/rAT57qA.png"></img>
+                <a href="https://www.amazon.com/gp/product/0393054772/ref=dbs_a_def_rwt_bibl_vppi_i4"><img src={cradle}></img>
                   <div class="middle">
                     <div class="text">buy</div>
                   </div></a>
               </div>
               <div className='book bmb'>
-                <a href="https://www.amazon.com/gp/product/1743218443/ref=dbs_a_def_rwt_bibl_vppi_i5"><img src="https://imgur.com/0ykSJMN.png" alt='fork in road book cover'></img>
+                <a href="https://www.amazon.com/gp/product/1743218443/ref=dbs_a_def_rwt_bibl_vppi_i5"><img src={fork}></img>
                   <div class="middle">
                     <div class="text">buy</div>
                   </div></a>
               </div>
 
               <div className='book brb'>
-                <a href="https://www.amazon.com/Jimmy-Neurosis-Memoir-James-Oseland/dp/0062267361/ref=sxts_sxwds-bia-wc-p13n1_0?crid=2PUBFRZHQT9TE&cv_ct_cx=jimmy+neurosis&dchild=1&keywords=jimmy+neurosis&pd_rd_i=0062267361&pd_rd_r=e19fa31a-4d7f-4f8b-8dbf-425c9615a652&pd_rd_w=muLPe&pd_rd_wg=ccBuf&pf_rd_p=1835a2a9-7ed8-48dc-ad07-fcd7527bd2bc&pf_rd_r=651P663Q3K24S1H5DVSH&psc=1&qid=1607723919&sprefix=jimmy+neur%2Caps%2C247&sr=1-1-80ba0e26-a1cd-4e7b-87a0-a2ffae3a273c"><img src="https://imgur.com/XsxQPL2.png" lt='jimmy neurosis book cover'></img>
+                <a href="https://www.amazon.com/Jimmy-Neurosis-Memoir-James-Oseland/dp/0062267361/ref=sxts_sxwds-bia-wc-p13n1_0?crid=2PUBFRZHQT9TE&cv_ct_cx=jimmy+neurosis&dchild=1&keywords=jimmy+neurosis&pd_rd_i=0062267361&pd_rd_r=e19fa31a-4d7f-4f8b-8dbf-425c9615a652&pd_rd_w=muLPe&pd_rd_wg=ccBuf&pf_rd_p=1835a2a9-7ed8-48dc-ad07-fcd7527bd2bc&pf_rd_r=651P663Q3K24S1H5DVSH&psc=1&qid=1607723919&sprefix=jimmy+neur%2Caps%2C247&sr=1-1-80ba0e26-a1cd-4e7b-87a0-a2ffae3a273c"><img src={jim}></img>
                   <div class="middle">
                     <div class="text">buy</div>
                 </div></a>
