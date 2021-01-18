0db0fc5b9811665974007ed85f3e8e881dd1589f
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2021-01-18 17:44:03 -0500

    yarn build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
-// import './fonts/MundoSansPro-Medium.woff'
 import './index.css';
 import App from './App';
 import './MyFontsWebfontsKit.css';
